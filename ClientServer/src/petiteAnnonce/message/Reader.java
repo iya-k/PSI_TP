fd303b81de5889cef46e64408aa77ffc1c6144a9
mmekaba <mmekaba@169.254.228.95>
2019-11-18 16:58:50 +0100

    Classe reader pour la communication des messages

--- ClientServer/src/petiteAnnonce/message/Reader.java
+++ ClientServer/src/petiteAnnonce/message/Reader.java
@@ -1,79 +1,65 @@
 package petiteAnnonce.message;
- 
+
 import java.io.*;
 import java.net.*;
 import java.util.Scanner;
- 
+
 /**
  * @author KABA
  *
  */
 public class Reader{
-    private static Socket socket;
-    private String userName;
-    
+	private static Socket socket;
+	private String userName;
+
 	/*
 	 * public Reader(Socket socket, String client) { this.socket = socket;
 	 * this.userName = client;
 	 * 
 	 * }
 	 */
-    
-    public static void main(String[] args) throws IOException {
+
+	public static void main(String[] args) throws IOException {
 
 
 		final PrintWriter writer;
 		final BufferedReader reader;
 		final Scanner sc = new Scanner(System.in);
-		
+
 		socket = new Socket("localhost",2000);
 		
-		
+		System.out.println("------------------ Client started ----------------");
+
+		OutputStream output;
 		try {
-			OutputStream output = socket.getOutputStream();
+			output = socket.getOutputStream();
+
 			writer = new PrintWriter(output, true);
 			InputStream input = socket.getInputStream();
 			reader = new BufferedReader(new InputStreamReader(input));
 
-			Thread send = new Thread(new Runnable() {
-				String message;
-				@Override
-				public void run() {
-					message = sc.nextLine();
-					writer.println(message);
-					writer.flush();
-				}
-			});
-			send.start();
-
-
-			Thread receive = new Thread(new Runnable() {
-				String message;
-				@Override
-				public void run() {
-					try {
-						message = reader.readLine();
-						while(message != null) {
-							System.out.println("[ Serveur ]: "+message);
-							message = reader.readLine();
-						}
-						System.out.println("Aurevoir");
-						writer.close();
-						socket.close();
-					} catch (IOException e) {
-						System.out.println("Error reader");
-						e.printStackTrace();
-					}
-					
-				}
-			});
-			receive.start();
-
-		} catch (IOException ex) {
-			System.out.println("Error getting output/input stream: " + ex.getMessage());
-			ex.printStackTrace();
+			
+			
+			while (true) {
+				//message = "";
+				
+				String message = reader.readLine();
+				System.out.println("\n[Serveur ]: "+message);
+				
+				System.out.print("\nMoi:");
+				message = sc.nextLine();
+				writer.println(message);
+				
+			}
+			
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
 		}
-
+		//writer.close();
+        //socket.close();
 		
-    }
+	}
+
+
 }
